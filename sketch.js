f1b35dc7ffc9a2e765776e23aa6bce0ce075b1d8
DAASHeo <hibstar719@naver.com>
2022-12-02 15:33:06 +0900

    설명 주석 추가

--- sketch.js
+++ sketch.js
@@ -6,8 +6,7 @@ function setup() {
   createCanvas(640, 440);
   wave = new p5.Oscillator();
   wave.setType('sine');
-  //wave.start();
-  wave.freq(440);
+  wave.freq(440); //440 4옥타브 라(가온 다) -> 기준점
   wave.amp(0); //volume
 
   button = createButton('play/pause');
